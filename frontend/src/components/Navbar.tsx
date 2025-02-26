b262047cb424a5f53567bdab3d25e2cc21bd905f
SDxBacon <z141134553@gmail.com>
2025-02-27 00:44:13 +0800

    feat: 🎸 implement UI i18n language changed

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,30 +1,38 @@
-import { Avatar, Select, SelectItem } from "@heroui/react";
+import { useEffect, useState } from "react";
+import { Select, SelectItem } from "@heroui/react";
+import { useTranslation } from "react-i18next";
 import GitHubButton from "./GitHubButton";
 import NexusModsButton from "./NexusModsButton";
 
-const animals = [
+const i18nLanguages = [
   { key: "en", label: "English" },
-  { key: "zh-tw", label: "繁體中文" },
-  { key: "zh-cn", label: "简体中文" },
+  { key: "zh_tw", label: "繁體中文" },
+  { key: "zh_cn", label: "简体中文" },
   { key: "ja", label: "日本語" },
   { key: "ko", label: "한국어" },
 ];
 
 function Navbar() {
+  const { t, i18n } = useTranslation();
+
+  const [usedLanguage, setUsedLanguage] = useState(i18nLanguages[0].key);
+
+  if (i18n.language !== usedLanguage) i18n.changeLanguage(usedLanguage);
+
   return (
     <div className="w-full flex items-center justify-end gap-2">
       {/* Language */}
       <Select
         className="max-w-xs"
-        label="Favorite Animal"
-        placeholder="Select an animal"
-        // selectedKeys={value}
+        label={t("LABEL_I18N_LANGUAGE")}
+        size="sm"
+        value={usedLanguage}
         variant="bordered"
         labelPlacement="outside-left"
-        // onSelectionChange={setValue}
+        onChange={(e) => setUsedLanguage(e.target.value)}
       >
-        {animals.map((animal) => (
-          <SelectItem key={animal.key}>{animal.label}</SelectItem>
+        {i18nLanguages.map((option) => (
+          <SelectItem key={option.key}>{option.label}</SelectItem>
         ))}
       </Select>
       {/* Nexus Mod Home Page */}
